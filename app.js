fa3bc82484232a7af9eb1d2811b1e3ab64675046
Kade <kadekorn@gmail.com>
2024-01-14 18:16:00 -0700

    I have, one resisted a bad urge currently, and two created pomodoro very quick after creating my stop watch. I understand my code a bit more

---
+++ app.js
@@ -0,0 +1,56 @@
+const startButton = document.querySelector(".stopwatch__start");
+const stopButton = document.querySelector(".stopwatch__stop");
+const resetButton = document.querySelector(".stopwatch__reset");
+
+const minutesElement = document.querySelector(".timer__minutes");
+const secondsElement = document.querySelector(".timer__seconds");
+const millisecondsElement = document.querySelector(".timer__milliseconds");
+
+let intervalId = null;
+let elapsedTime = 1500000; // Time in milliseconds
+
+function updateTimerDisplay() {
+  const minutes = Math.floor(elapsedTime / 60000);
+  const seconds = Math.floor((elapsedTime % 60000) / 1000);
+  const milliseconds = elapsedTime % 1000;
+
+  setTimerDisplay(minutes, seconds, milliseconds);
+}
+
+function setTimerDisplay(minutes, seconds, milliseconds) {
+  minutesElement.textContent = padWithZero(minutes, 2);
+  secondsElement.textContent = padWithZero(seconds, 2);
+  millisecondsElement.textContent = padWithZero(milliseconds, 3);
+}
+
+function padWithZero(number, length) {
+  let str = "" + number;
+  while (str.length < length) {
+    str = "0" + str;
+  }
+  return str;
+}
+
+function startTimer() {
+  intervalId = setInterval(() => {
+    elapsedTime -= 10; // Increment time
+    updateTimerDisplay(); // Update the timer display
+    startButton.disabled = true; // Re-enable the start button
+  }, 10);
+}
+
+function stopTimer() {
+  clearInterval(intervalId);
+  startButton.disabled = false; // Re-enable the start button
+}
+
+function resetTimer() {
+ 
+  elapsedTime = 1500000;
+  startButton.disabled = false; // Re-enable the start button
+  setTimerDisplay(25, 0, 0);
+}
+
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);
